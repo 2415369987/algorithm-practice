3a78f3bd445aeeeab1ce13f42ea023f7ac3ddb9c
Junjie_Zhu <160152901+2415369987@users.noreply.github.com>
2025-03-31 16:39:28 +0800

    Create code.cpp

---
+++ problem-set/dp/state-comprassion-dp/蒙德里安的梦想/code.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+const int N = 12;
+int n, m;
+typedef long long ll;
+ll f[N][1 << N];
+bool st[N];
+int main()
+{
+    while (cin >> n >> m, n || m) {
+        for (int i = 0; i < 1 << n; i ++ ) {
+            int cnt = 0;
+            bool is_valid = true;
+            for (int j = 0; j < n; j ++ ) {
+                if (i >> j & 1) {
+                    if (cnt & 1) {
+                        is_valid = false;
+                        break;
+                    } 
+                    // cnt = 0;
+                } else cnt ++;
+            }
+            if (cnt & 1) is_valid = false;
+            st[i] = is_valid;
+        }
+        
+        memset(f, 0, sizeof f);
+        
+        f[0][0] = 1;
+        for (int i = 1; i <= m; i ++ ) {
+            for (int j = 0; j < 1 << n; j ++ ) {
+                for (int k = 0; k < 1 << n; k ++ ) {
+                    if ((j & k) == 0 && st[j | k]) {
+                        f[i][j] += f[i - 1][k];
+                    }
+                } 
+            }
+        }
+        cout << f[m][0] << endl;
+    }
+    return 0;
+}
